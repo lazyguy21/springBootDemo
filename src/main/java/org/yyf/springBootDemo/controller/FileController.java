b3a262501c83f23eee04911bd937953cebf38a1c
tobi <asdf@asdf>
2018-05-21 16:31:33 +0800

    等等等

--- src/main/java/org/yyf/springBootDemo/controller/FileController.java
+++ src/main/java/org/yyf/springBootDemo/controller/FileController.java
@@ -4,7 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.common.io.Files;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +19,22 @@ public class FileController {
   public void uploadFile(MultipartFile file) throws IOException {
     byte[] bytes = file.getBytes();
     log.info(new String(bytes));
-    log.info(file.getContentType()+file.getName()+file.getOriginalFilename()+file.getSize());
+    log.info(file.getContentType() + file.getName() + file.getOriginalFilename() + file.getSize());
+  }
+
+  @RequestMapping("/file2")
+  public void uploadFile2(MultipartFile file) throws IOException {
+    byte[] bytes = file.getBytes();
+    log.info(new String(bytes));
+    log.info(file.getContentType() + file.getName() + file.getOriginalFilename() + file.getSize());
+    String fileName = UUID.randomUUID().toString();
+    String path="/apps/file/"+fileName;
+    final File newFile = new File(path);
+    try {
+      Files.write(bytes, newFile);
+    } catch (IOException fileIoEx) {
+      log.error("ERROR trying to write to file '" + fileName + "' - "
+          + fileIoEx.toString());
+    }
   }
 }
