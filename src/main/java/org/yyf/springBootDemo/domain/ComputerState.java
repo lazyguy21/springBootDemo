d4841550600bcccf0bd279812883233236d2f1a7
tobi <asdf@asdf>
2018-07-02 11:44:53 +0800

    assinable

--- src/main/java/org/yyf/springBootDemo/domain/ComputerState.java
+++ src/main/java/org/yyf/springBootDemo/domain/ComputerState.java
@@ -3,24 +3,44 @@ package org.yyf.springBootDemo.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ComputerState implements CodeInsideEnum {
-    OPEN(10),         //开启
-    CLOSE(11),         //关闭
-    OFF_LINE(12),     //离线
-    FAULT(200),     //故障
-    UNKNOWN(255);     //未知
+  OPEN(10),
+  //开启
+  CLOSE(11),
+  //关闭
+  OFF_LINE(12),
+  //离线
+  FAULT(200),
+  //故障
+  UNKNOWN(255);     //未知
 
-    private int code;
-    ComputerState(int code) { this.code = code; }
+  private int code;
 
-    @JsonValue
-    @Override
-    public int getCode() { return this.code; }
+  ComputerState(int code) {
+    this.code = code;
+  }
 
+  @JsonCreator
+  public static ComputerState valueOf(int code) {
+    ComputerState computerState = CodeEnumUtil.codeOf(ComputerState.class, code);
+    return computerState;
+  }
 
-    @JsonCreator
-    public static ComputerState valueOf(int code) {
-        ComputerState computerState = CodeEnumUtil.codeOf(ComputerState.class, code);
-        return computerState;
-    }
+  public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+    boolean assignableFrom1 = CodeInsideEnum.class.isAssignableFrom(ComputerState.class);
+    boolean assignableFrom2 = ArrayList.class.isAssignableFrom(List.class);
+    System.out.println(assignableFrom);
+    System.out.println(assignableFrom1);
+    System.out.println(assignableFrom2);
+//    System.out.println(ComputerState.class.newInstance() instanceof (CodeInsideEnum.class));
+  }
+
+  @JsonValue
+  @Override
+  public int getCode() {
+    return this.code;
+  }
 }
--- src/main/java/org/yyf/springBootDemo/util/ServletInitializer.java
+++ src/main/java/org/yyf/springBootDemo/util/ServletInitializer.java
@@ -1,13 +1,13 @@
-package org.yyf.springBootDemo.util;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.yyf.springBootDemo.configuration.DemoApplication;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(DemoApplication.class);
-  }
-}
+//package org.yyf.springBootDemo.util;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.yyf.springBootDemo.configuration.DemoApplication;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//  @Override
+//  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//    return application.sources(DemoApplication.class);
+//  }
+//}
